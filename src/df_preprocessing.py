f4c58b31e3bb562a494b2db5e3c5457e4f9e32cf
simone <simonebenitozzi@gmail.com>
2022-08-12 19:23:29 +0200

    processed non-competitive data

--- src/df_preprocessing.py
+++ src/df_preprocessing.py
@@ -1,23 +1,66 @@
 import pandas as pd
-import datetime
+from datetime import datetime
 
-results_df = pd.read_csv("CorriBicoccaResults.csv")
+def time_to_minutes(time):
+    pt = datetime.strptime(time,'%H:%M:%S')
+    minutes = round(pt.second/60,2) + pt.minute + pt.hour*60    
+    return minutes
 
-seconds_list = []
+def edit_lines(file_lines, min_cols):
+    
+    for i in range(len(file_lines)):
+        line = file_lines[i]
+        line_parts = line.split(" ")
+        
+        time_index = -2 if min_cols==3 else -1
+        time = line_parts[time_index].rstrip()
+        
+        if len(line_parts) > min_cols:
+            team = line_parts[0:time_index-1]
+            team = ' '.join(team)
+        else:
+            team = "INDIVIDUALE"
+        
+        file_lines[i] = team + "," + str(time_to_minutes(time)) + "\n"
 
-for i in range(len(results_df)):
-    pt = datetime.strptime(results_df.loc[i]["time"],'%H:%M:%S')
-    total_seconds = pt.second + pt.minute*60 + pt.hour*3600
-    seconds_list.append(total_seconds)
+    return file_lines
 
-results_df["seconds"] = seconds_list
-results_df["minutes"] = round(results_df["seconds"]/60, 2)
+# TODO: use os.path
 
-results_df.sort_values("seconds", inplace=True)
-results_df = results_df.reset_index(drop=True)
-results_df.drop(results_df.columns[0], axis=1, inplace=True)
+source = ".\\data\\raw_data\\2019_10km_noncomp"
+target = ".\\data\\2019_10km_noncomp.csv"
+file = open(source, "r")
+file_lines = file.readlines()
 
-results_df.to_csv("CorriBicoccaResults.csv", index=False)
+if "2021" in source:
+    min_cols = 3
+else:
+    min_cols = 2
 
-print(len(results_df))
-results_df.head()
+new_lines = edit_lines(file_lines,min_cols)
+
+file = open(target, "w")
+file.writelines(new_lines)
+file.close()
+
+# results_df = pd.read_csv(filename)
+
+# for each line:
+    # read and split by " "
+    # use negative indexes for: (average if 2021), time, nation, (team: [0:-3 or -4])
+    # store all in variables
+    # rewrite with commas (ignore nation)
+
+
+# results_df["minutes"] = minutes_list
+
+# results_df.sort_values("seconds", inplace=True)
+# results_df = results_df.reset_index(drop=True)
+# results_df.drop(results_df.columns[0], axis=1, inplace=True)
+
+# results_df.to_csv("CorriBicoccaResults.csv", index=False)
+
+# print(len(results_df))
+# results_df.head()
+
+
